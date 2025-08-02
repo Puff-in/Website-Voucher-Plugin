cb1bdc9ec926c85a2143762ebe0bbf6117ffd71d
Puffin <128737516+Puff-in@users.noreply.github.com>
2025-08-02 17:35:45 -0400

    Initial Commit

---
+++ src/main/java/com/puffin/websitevoucher/WebsiteVoucher.java
@@ -0,0 +1,90 @@
+package com.puffin.websitevoucher;
+
+import com.eu.habbo.Emulator;
+import com.eu.habbo.habbohotel.commands.CommandHandler;
+import com.eu.habbo.habbohotel.users.Habbo;
+import com.eu.habbo.plugin.EventHandler;
+import com.eu.habbo.plugin.EventListener;
+import com.eu.habbo.plugin.HabboPlugin;
+import com.eu.habbo.plugin.events.emulator.EmulatorLoadedEvent;
+import com.puffin.websitevoucher.commands.AddWebsiteVoucherCommand;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class WebsiteVoucher extends HabboPlugin implements EventListener {
+    public static WebsiteVoucher INSTANCE = null;
+
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+        Emulator.getPluginManager().registerEvents(this, this);
+
+        if (Emulator.isReady) {
+            this.checkDatabase();
+        }
+
+        Emulator.getLogging().logStart("[WebsiteVoucher] Started Website Voucher Plugin!");
+    }
+
+    @Override
+    public void onDisable() {
+        Emulator.getLogging().logShutdownLine("[WebsiteVoucher] Stopped Website Voucher Plugin!");
+    }
+
+    @EventHandler
+    public static void onEmulatorLoaded(EmulatorLoadedEvent event) {
+        INSTANCE.checkDatabase();
+    }
+
+    @Override
+    public boolean hasPermission(Habbo habbo, String s) {
+        return false;
+    }
+
+    private void checkDatabase() {
+        boolean reloadPermissions = false;
+
+        try (Connection connection = Emulator.getDatabase().getDataSource().getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("ALTER TABLE  `emulator_texts` CHANGE  `value`  `value` VARCHAR( 4096 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL");
+        } catch (SQLException e) {
+        }
+
+        Emulator.getTexts().register("commands.keys.cmd_shopvoucher", "shopvoucher;voucher");
+        Emulator.getTexts().register("commands.description.cmd_shopvoucher", ":shopvoucher <code> <amount>");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.missing_parameters", "Usage: :shopvoucher <code> <amount>");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.invalid_amount", "The amount you use must be a valid number");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.invalid_code_length", "Voucher code must be between 1 and 3 numbers long");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.invalid_amount_range", "Amount must be between 1 and 500");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.positive_amount", "The amount you use must be a positive number");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.code_exists", "A voucher with code '%code%' already exists");
+        Emulator.getTexts().register("commands.error.cmd_shopvoucher.database_error", "Error creating shop voucher");
+        Emulator.getTexts().register("commands.success.cmd_shopvoucher.created", "You have successfully created the voucher '%code%' for %amount% USD");
+        
+        reloadPermissions = this.registerPermission("cmd_shopvoucher", "'0', '1'", "0", reloadPermissions);
+        
+        CommandHandler.addCommand(new AddWebsiteVoucherCommand("cmd_shopvoucher", Emulator.getTexts().getValue("commands.keys.cmd_shopvoucher").split(";")));
+
+        if (reloadPermissions) {
+            Emulator.getGameEnvironment().getPermissionsManager().reload();
+        }
+    }
+
+    private boolean registerPermission(String name, String options, String defaultValue, boolean defaultReturn) {
+        try (Connection connection = Emulator.getDatabase().getDataSource().getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("ALTER TABLE  `permissions` ADD  `" + name + "` ENUM(  " + options + " ) NOT NULL DEFAULT  '" + defaultValue + "'")) {
+                statement.execute();
+                return true;
+            }
+        } catch (SQLException e) {
+        }
+
+        return defaultReturn;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Don't run this separately");
+    }
+}
---
+++ src/main/java/com/puffin/websitevoucher/commands/AddWebsiteVoucherCommand.java
@@ -0,0 +1,90 @@
+package com.puffin.websitevoucher.commands;
+
+import com.eu.habbo.Emulator;
+import com.eu.habbo.habbohotel.commands.Command;
+import com.eu.habbo.habbohotel.gameclients.GameClient;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AddWebsiteVoucherCommand extends Command {
+
+    public AddWebsiteVoucherCommand(String permission, String[] keys) {
+        super(permission, keys);
+    }
+
+    @Override
+    public boolean handle(GameClient gameClient, String[] strings) throws Exception {
+        if (strings.length != 3) {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.error.cmd_shopvoucher.missing_parameters"));
+            return true;
+        }
+
+        String voucherCode = strings[1];
+        int amount;
+
+        if (voucherCode.trim().length() < 1 || voucherCode.trim().length() > 3) {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.error.cmd_shopvoucher.invalid_code_length"));
+            return true;
+        }
+
+        try {
+            amount = Integer.parseInt(strings[2]);
+        } catch (NumberFormatException e) {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.error.cmd_shopvoucher.invalid_amount"));
+            return true;
+        }
+
+        if (amount <= 0 || amount > 500) {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.error.cmd_shopvoucher.invalid_amount_range"));
+            return true;
+        }
+
+        if (voucherCodeExists(voucherCode)) {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.error.cmd_shopvoucher.code_exists").replace("%code%", voucherCode));
+            return true;
+        }
+
+        if (createVoucher(voucherCode, amount)) {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.success.cmd_shopvoucher.created")
+                    .replace("%code%", voucherCode)
+                    .replace("%amount%", String.valueOf(amount)));
+        } else {
+            gameClient.getHabbo().whisper(Emulator.getTexts().getValue("commands.error.cmd_shopvoucher.database_error"));
+        }
+
+        return true;
+    }
+
+    private boolean voucherCodeExists(String code) {
+        try (Connection connection = Emulator.getDatabase().getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT id FROM website_shop_vouchers WHERE code = ?")) {
+            
+            statement.setString(1, code);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next();
+            }
+        } catch (SQLException e) {
+            Emulator.getLogging().logSQLException(e);
+            return true;
+        }
+    }
+
+    private boolean createVoucher(String code, int amount) {
+        try (Connection connection = Emulator.getDatabase().getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO website_shop_vouchers (code, amount, max_uses, use_count) VALUES (?, ?, 1, 0)")) {
+            
+            statement.setString(1, code);
+            statement.setInt(2, amount);
+            
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            Emulator.getLogging().logSQLException(e);
+            return false;
+        }
+    }
+}
